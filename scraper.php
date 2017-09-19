903ab6b04f334cc916a8d8996112750a5f6e54bf
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-19 15:29:59 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -9,8 +9,8 @@ $link =	file_get_html($Link);
 $loopnumberforpagintion = $link->find("//[@id='w1']/div/div[1]/div[1]/div/b[2]",0)->plaintext;
 $text = str_replace(',', '', $loopnumberforpagintion);
 $paginations = $text  / 15 + 1;
-echo $paginations;
-//$loop =  (int)$paginations;
+
+echo $loop =  (int)$paginations;
 $loop =  1;
 
 
