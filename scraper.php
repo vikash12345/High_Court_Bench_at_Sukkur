b46e2b695a91a1786ada4c806bd0e03d5af8cf46
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-19 15:26:02 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -26,7 +26,7 @@ for($i = 1; $i <= $loop; $i++)
 				if($num != null)
 					{
 						$num        = $element->find('td[1]',0)->plaintext;
-						echo '||'.$num;
+						echo '|'.$num;
 						$sourcename 	= 'High Court of Sindh, Bench at Sukkur';
 						$casename    	= $element->find('td[2]',0)->plaintext;
 						$caseno        	= $element->find('td[3]',0)->plaintext;
